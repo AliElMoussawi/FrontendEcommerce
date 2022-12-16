d5ac14e5b02184870a7407b75ea3859d25e9aaf5
Ali Moussawi <amm97@mail.aub.edu>
2022-12-16 02:33:49 +0200

    retrieve data using open source API and display them

--- src/Component/Products.jsx
+++ src/Component/Products.jsx
@@ -28,11 +28,29 @@ const Products=()=>{
     const Loading =()=>{return(<>Loading...</>)}
     const ShowProducts=()=>{
         return (<>
-        <div className="buttons">
-            <button className="btn btn-outline-dark">All</button>
-            <button className="btn btn-outline-dark">Men's Clothing</button>
-   
-        </div></>)
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+            <button className="btn btn-outline-dark me-2">All</button>
+            <button className="btn btn-outline-dark me-2">Category 1</button>
+            <button className="btn btn-outline-dark me-2">Category 2</button>
+            <button className="btn btn-outline-dark me-2">Category 3</button>
+            <button className="btn btn-outline-dark me-2">Category 4</button>
+            <button className="btn btn-outline-dark me-2">Category 5</button>
+        </div>
+        {filter.map((product)=>{return(<>
+            <div className="col-md-3">
+            <div class="card h-100 text-center p-4" key={product.id} >
+        <img src={product.image} class="card-img-top" alt={product.title}/>
+        <div class="card-body">
+        <h5 class="card-title">{product.title}</h5>
+        <p class="card-text">${product.price}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div></div>
+</div>
+         </>)
+    
+    
+    })}
+        </>)
     }
     return (
 
