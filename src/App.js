ec770337459b650fd3e4fb327d69f82570984f1e
Ali Moussawi <amm97@mail.aub.edu>
2022-12-16 14:48:01 +0200

    checkout and login

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import Home from './Component/Home';
 import {Routes,Route} from 'react-router-dom';
 import Products from './Component/Products';
 import Product from './Component/Product';
+import Cart from './Component/Cart';
+import Login from './Component/Login';
+import Checkout from './Component/Checkout';
+
 function App() {
   return (
    
     <> 
+  
     <Navbar/>
     <Routes>
     <Route exact path='/' element={<Home/>} />
       <Route exact path='/products' element={<Products/>} />
       <Route exact path='/products/:id' element={<Product/>} />
+      <Route exact path='/cart' element={<Cart/>} />
+      <Route exact path='/checkout' element={<Checkout/>} />
 
     </Routes>
     </>
--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import{useSelector} from 'react-redux'
-const Cart =()=>{
-    
-    return (<div>
+import {useDispatch} from 'react-redux'
+import {delCart} from '../redux/action/index'
+import { NavLink } from 'react-router-dom';
 
-    </div>);
+const Cart =()=>{
+    const state =useSelector((state)=>state.handleCart)
+    const dispatch=useDispatch()
+    const handleClose=(item)=>{dispatch(delCart(item));}
+    const cartItems=(cartItem)=>{
+    return (
+      
+    <div className="px-4 my-5 bg-light rounded-3" key={cartItem.id}>
+<div className="container py-4">
+    <button onClick={()=>handleClose(cartItem)}className="btn-close float-end"aria-label="Close"></button>
+    <div className="row Justify-context-center">
+        <div className="col-md-4">
+            <img src={cartItem.image} alt={cartItem.title} height="200px" width="180px"/>
+          </div>
+            <div className="col-md-4">
+            <h3>{cartItem.title}</h3>
+            <p className="lead fw-bold">${cartItem.price}</p>
+        </div>
+    </div>
+</div>
+    </div>);}
+    const emptyCart=()=>{return (
+        <div className="px-4 my-5 bg-light rounded-4">
+
+            <div className="container py-4">
+                <h3>Your Cart is Empty</h3>
+            </div>
+        </div>);}
+    const button=()=>{
+        return(
+            <div className="container">
+                <div className="row">
+                    <NavLink to="/checkout" className="btn">Proceed to checkout</NavLink>
+                </div>
+            </div>
+    );}
+    return(<>{state.length ===0 && emptyCart()}
+    {state.length !==0 && state.map(cartItems)}
+    {state.length !==0 && button()}</>)
 }
+
 export default Cart;
