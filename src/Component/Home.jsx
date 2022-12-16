9af5e10c956edd04fb218e1f96c9823169e2becb
Ali Moussawi <amm97@mail.aub.edu>
2022-12-16 02:10:52 +0200

    first commit

---
+++ src/Component/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Products from './Products';
+const Home =()=> {
+
+return (
+    <div className="hero">
+<div className="card bg-dark text-white border-0">
+  <img src="public/assets/ecommerce.jpg" className="card-img" alt="Background" height="550px" />
+  <div className="card-img-overlay">
+    <div className="container"> <h5 className="card-title display-3 fw-bolder mb-0"t>New Products</h5>
+    <p className="card-text">CHECK OUT ALL OUR NEW PRODUCTS </p>
+    </div>
+     </div>
+    </div>
+    <Products/>
+</div>
+);
+};
+export default Home;
